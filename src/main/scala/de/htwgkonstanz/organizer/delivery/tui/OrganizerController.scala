0550f2dd436b5d3a816a9498e70cf4f382c7bffd
Vladyslav Pekker <agilesteel@gmail.com>
2011-11-24 12:40:33 +0100

    Pimped the tui view

--- src/main/scala/de/htwgkonstanz/organizer/delivery/tui/OrganizerController.scala
+++ src/main/scala/de/htwgkonstanz/organizer/delivery/tui/OrganizerController.scala
@@ -7,25 +7,6 @@ class OrganizerController private (val model: Model, val source: String, val tar
   def areControllsEnabled = isSourceSet
   def isSourceSet = !source.isEmpty
 
-  def menu = {
-    val beginning = List(
-      "Here are your options:",
-      "source | s",
-      "target | t")
-
-    val controlls = List(
-      "parse | pa",
-      "preview | p",
-      "organize | o")
-
-    val ending = List(
-      "start over | so",
-      "quit | q")
-
-    if (areControllsEnabled) beginning ::: controlls ::: ending
-    else beginning ::: ending
-  }
-
   def status = List(
     "Source: " + source,
     "Target: " + target)
--- src/main/scala/de/htwgkonstanz/organizer/delivery/tui/OrganizerView.scala
+++ src/main/scala/de/htwgkonstanz/organizer/delivery/tui/OrganizerView.scala
@@ -1,65 +1,107 @@
 package de.htwgkonstanz.organizer.delivery.tui
 
-class OrganizerView(val controller: Controller) {
-  def display() {
+import Console._
+import OrganizerView._
+
+object OrganizerView {
+  implicit val defaultColor = RESET
+}
+
+class OrganizerView(var controller: Controller) {
+  def display(): Unit = {
     presentGreeting()
-    presentMenu()
+    presentControlls()
   }
 
   private def presentGreeting() {
-    present("Welcome to song organizer!")
-    present("Hold on a sec, I am initializing the neccessary components...\n")
+    present("Welcome to song organizer!")(YELLOW)
+    present("Hold on a sec, I am initializing the neccessary components...")(YELLOW)
   }
 
-  private def present(x: Any = "") {
-    println(x)
+  private def present(x: Any = "")(implicit color: String) {
+    println(color + x + RESET + "\n")
   }
 
-  private def presentMenu() {
-    var c = controller
-
+  private def presentControlls() {
     while (true) {
-      presentList(c.menu)
-      val r = readLine
-      r match {
-        case "source" | "s" => {
-          present("In which directory are your unorganized songs?")
-          c = c.setSource(readLine)
-        }
-        case "target" | "t" => {
-          present("Where should I organize your songs to?")
-          c = c.setTarget(readLine)
-        }
-        case "parse" | "pa" => {
-          if (c.areControllsEnabled) {
-            present("Look what I've found")
-            presentList(c.parse)
-          } else
-            Console.err.println("I beg your pardon?")
-        }
-        case "preview" | "p" =>
-          if (c.areControllsEnabled) {
-            present("This is how your new directory structure will look like:")
-            presentList(c.preview)
-          } else
-            Console.err.println("I beg your pardon?")
-        case "organize" | "o" => {
-          if (c.areControllsEnabled) {
-
-            present("This can take a while. You might wanna grab a snack...")
-            c.organize()
-          } else
-            Console.err.println("I beg your pardon?")
-        }
-        case "start over" | "so" => c = c.startOver
-        case "quit" | "q" | "exit" | "e" => return
-        case unknownCommand => Console.err.println("I beg your pardon?")
-      }
-      presentList(c.status)
+      presentStatus()
+      presentMenu()
+      controller = waitForInput()
     }
   }
 
-  private def presentList(list: scala.collection.GenTraversable[_]) {
-    present(list mkString "\n")
+  private def presentStatus() {
+    presentList(controller.status)(GREEN)
+  }
+
+  private def presentMenu() {
+    presentList(menu)
+  }
+
+  private def menu = {
+    val beginning = List(
+      "Here are your options:",
+      "s  | source\t\tChange the source directory",
+      "t  | target\t\tChange the target directory")
+
+    val controlls = List(
+      "pa | parse\t\tParse the source directory",
+      "p  | preview\t\tPreview the target directory",
+      "o  | organize\t\tCopy files from the source directory into the target directory")
+
+    val ending = List(
+      "so | start over\t\tClear source and target directories",
+      "q  | quit\t\tClose the program",
+      "e  | exit\t\tClose the program")
+
+    if (controller.areControllsEnabled) beginning ::: controlls ::: ending
+    else beginning ::: ending
+  }
+
+  private def presentList(list: scala.collection.GenTraversable[_])(implicit color: String) {
+    present(list mkString "\n")(color)
+  }
+
+  private def waitForInput() = readLine match {
+    case "source" | "s" => {
+      present("In which directory are your unorganized songs?")
+      controller.setSource(readLine)
+    }
+    case "target" | "t" => {
+      present("Where should I organize your songs to?")
+      controller.setTarget(readLine)
+    }
+    case "parse" | "pa" => {
+      if (controller.areControllsEnabled) {
+        present("Look what I've found")
+        presentList(controller.parse)
+      } else
+        present("I beg your pardon?")(RED)
+      controller
+    }
+    case "preview" | "p" =>
+      if (controller.areControllsEnabled) {
+        present("This is how your new directory structure will look like:")
+        presentList(controller.preview)
+      } else
+        present("I beg your pardon?")(RED)
+      controller
+    case "organize" | "o" => {
+      if (controller.areControllsEnabled) {
+        present("This can take a while. You might wanna grab a snack...")
+        controller.organize()
+      } else
+        present("I beg your pardon?")(RED)
+      controller
+    }
+    case "start over" | "so" => controller.startOver
+    case "quit" | "q" | "exit" | "e" => {
+      System.exit(0)
+      controller
+    }
+    case unknownCommand => {
+      present("I beg your pardon?")(RED)
+      controller
+    }
   }
 }
