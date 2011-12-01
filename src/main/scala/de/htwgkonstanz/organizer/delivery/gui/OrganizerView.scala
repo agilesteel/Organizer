dff1298c9128b1cdedbc797bd73a5619387155cc
Vladyslav Pekker <agilesteel@gmail.com>
2011-12-01 18:13:21 +0100

    Added scroll panes

--- src/main/scala/de/htwgkonstanz/organizer/delivery/gui/OrganizerView.scala
+++ src/main/scala/de/htwgkonstanz/organizer/delivery/gui/OrganizerView.scala
@@ -16,7 +16,7 @@ class OrganizerView(private[this] var _controller: Controller) extends Frame wit
     publish(ControllerRefreshed)
   }
 
-  listenTo(this, source, target)
+  listenTo(this, source, target, parse, preview, organize)
   contents = new BoxPanel(Orientation.Vertical) {
     border = Swing.EmptyBorder(5, 5, 5, 5)
     contents += new Label("Source")
@@ -29,7 +29,8 @@ class OrganizerView(private[this] var _controller: Controller) extends Frame wit
       contents += target
       contents += new Button("Select")
     }
-    contents += new TextArea
+    contents += new ScrollPane(sourceResults)
+    contents += new ScrollPane(targetResults)
     contents += new FlowPanel {
       contents += parse
       contents += preview
@@ -48,30 +49,66 @@ class OrganizerView(private[this] var _controller: Controller) extends Frame wit
   lazy val parse = new Button("Parse") {
     enabled = controller.areControllsEnabled
   }
-  
+
   lazy val preview = new Button("Preview") {
     enabled = controller.areControllsEnabled
   }
-  
+
   lazy val organize = new Button("Organize") {
     enabled = controller.areControllsEnabled
   }
 
+  lazy val sourceResults = new TextArea
+  lazy val targetResults = new TextArea
+
   reactions += {
     case EditDone(`source`) => controller = controller.setSource(`source`.text)
     case EditDone(`target`) => controller = controller.setTarget(`target`.text)
-    case ControllerRefreshed => {
-      parse.enabled = controller.areControllsEnabled
-      preview.enabled = controller.areControllsEnabled
-      organize.enabled = controller.areControllsEnabled
+    case ButtonClicked(`parse`) => clearResults(); controller.parse.fold(presentError, presentList(sourceResults))
+    case ButtonClicked(`preview`) => clearResults(); controller.preview.fold(presentError, presentMap)
+    case ButtonClicked(`organize`) => clearResults(); controller.organize.fold(presentError, Unit => ())
+    case ControllerRefreshed => enableButtons()
+  }
+
+  private def enableButtons() {
+    parse.enabled = controller.areControllsEnabled
+    preview.enabled = controller.areControllsEnabled
+    organize.enabled = controller.areControllsEnabled
+  }
+
+  private def clearResults() {
+    sourceResults.text = ""
+    targetResults.text = ""
+  }
+
+  private def presentError(error: String) {
+    presentLine(sourceResults)(error)
+  }
+
+  private def presentLine(area: TextArea)(x: Any = "") {
+    presentSingleLine(area)(x + "\n")
+  }
+
+  private def presentSingleLine(area: TextArea)(x: Any = "") {
+    area.text += x.toString
+  }
+
+  private def presentList(area: TextArea)(list: scala.collection.GenTraversable[_]) {
+    presentLine(area)(list mkString "\n")
+  }
+
+  private def presentMap(map: scala.collection.GenMap[_, _]) {
+    for ((from, to) <- map) {
+      presentLine(sourceResults)(from)
+      presentLine(targetResults)(to)
     }
   }
 
   peer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
   centerOnScreen()
   pack()
+  maximize()
   title = "Organizer"
-  resizable = false
   visible = true
 }
 
